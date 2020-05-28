7e6983f25270ad48b25dd711e69b6d8c14128e38
Taein Kim <kti91@likelion.org>
2020-05-28 23:48:11 +0900

    0528 | added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Main from './components/Main'
+import About from './components/About'
+import axios from 'axios'
 function App() {
-	return <div className='App'></div>
+	const [posts, setPosts] = useState(null)
+	axios
+		.get('https://jsonplaceholder.typicode.com/posts')
+		.then((res) => res.data)
+		.then((data) => {
+			setPosts(data)
+			console.log(data)
+		})
+	return (
+		<div className='app'>
+			<Router>
+				<Link to='/'>Main</Link> | <Link to='/About'>About</Link>
+				<Route path='/About' component={About} />
+				<Route exact path='/' component={Main} />
+			</Router>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+	return (
+		<>
+			<h1>Main</h1>
+			<p>메인 페이지 입니다</p>
+		</>
+	)
+}
